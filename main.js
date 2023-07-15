9967d52f554a3c9579073c01ec7e224a437dbfba
cjy <202230337@email.daelim.ac.kr>
2023-07-16 05:03:19 +0900

    commit

--- main.js
+++ main.js
@@ -1,3 +1,17 @@
-import _ from "lodash"
+const obj = {
+	a: 1,
+	b: 2,
+	c: 3,
+}
 
-console.log('hello')
+const {a, b} = obj;
+console.log(a,b) // 1 2
+
+const {x = 4} =obj;
+console.log(x) // 4
+
+const {c: test} = obj;
+console.log(test) // 3
+
+const {c, ...rest} = obj;
+console.log(c, rest)
